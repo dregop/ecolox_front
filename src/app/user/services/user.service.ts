8ce7c30a26d82a9841e74bcba84ad5a64fff2c06
Dregop <pierredrege_2@hotmail.fr>
2023-08-31 01:14:32 +0200

    add signup/login capa, http interceptor and auth services and stuff

---
+++ src/app/user/services/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { shareReplay } from 'rxjs';
+import { API_URL } from 'src/app/env';
+import { User } from 'src/app/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getProfile() {
+    return this.http.get<User>(API_URL + '/get_profile')
+    .pipe(
+        shareReplay() // prevent multiple http call
+      );
+  }
+}
