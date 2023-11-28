cdc745ea783f3423264aa5452973e7fa44f12362
Dregop <pierredrege_2@hotmail.fr>
2023-11-28 18:26:55 +0100

    premier commit

---
+++ src/app/customComponents/toast/toast.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+export enum toastType {
+  Error,
+  Info,
+  Success
+}
+
+@Component({
+  selector: 'app-toast',
+  templateUrl: './toast.component.html',
+  styleUrls: ['./toast.component.scss']
+})
+export class ToastComponent {
+  public toastMessage = '';
+  constructor() {}
+ 
+  public handleToast(type: toastType, message: string) {
+      const toastDiv = document.getElementById('toast');
+      if (toastDiv) {
+        toastDiv.className = "show";
+        this.toastMessage = message;
+        switch(type) {
+          case toastType.Error:
+            toastDiv.style.backgroundColor = 'rgb(228, 60, 60)';
+            break;
+          case toastType.Success:
+            toastDiv.style.backgroundColor = 'rgb(175, 224, 175)';
+            break;
+          case toastType.Info:
+            toastDiv.style.backgroundColor = 'rgb(64, 128, 207)';
+            break;
+        }
+        setTimeout(function(){ toastDiv.className = toastDiv.className.replace("show", ""); }, 4000);
+      }
+    }
+}
